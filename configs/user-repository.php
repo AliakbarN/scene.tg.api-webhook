4c86c6f817a92785cdd9e795ef4cc6e089ed7dd9
Aliakbar <anurillahonov@gmail.com>
2024-04-19 22:22:45 +0500

    #fix commands autoload, default db conf

--- configs/user-repository.php
+++ configs/user-repository.php
@@ -5,8 +5,7 @@ return [
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'host'      => '127.0.0.1',
-    'port'      => 5432,
-    'dbname'    => 'scene',
-    'username'  => 'postgres',
-    'password'  => 'root123',
+    'dbname'    => '',
+    'username'  => '',
+    'password'  => '',
 ];
